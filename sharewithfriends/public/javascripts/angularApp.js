6c1c15a60aea1ab29e00b7ef0ead24133bffcf75
Callan Lamb <callan.lamb@gmail.com>
2016-11-23 09:01:34 -0500

    Inject ui-router dependency + sample button

--- sharewithfriends/public/javascripts/angularApp.js
+++ sharewithfriends/public/javascripts/angularApp.js
@@ -1,4 +1,4 @@
-var app = angular.module('shareWithFriends', ['ui.router']);
+var app = angular.module('shareWithFriends', ['ui.router', 'ui.bootstrap']);
 
 app.factory('posts', ['$http', function($http) {
 	var o = {
@@ -122,3 +122,26 @@ app.controller('PostsCtrl', [
   			posts.upvoteComment(post, comment);
   		};
 	}]);
+
+app.controller('ButtonsCtrl', function($scope) {
+	$scope.singleModel = 1;
+
+  $scope.radioModel = 'Middle';
+
+  $scope.checkModel = {
+    left: false,
+    middle: true,
+    right: false
+  };
+
+  $scope.checkResults = [];
+
+  $scope.$watchCollection('checkModel', function () {
+    $scope.checkResults = [];
+    angular.forEach($scope.checkModel, function (value, key) {
+      if (value) {
+        $scope.checkResults.push(key);
+      }
+    });
+  });
+});
