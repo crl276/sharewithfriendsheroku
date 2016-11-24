abd27ca2b4d6ec83a833a3d8a6196bf2c1ba7ecc
Callan Lamb <callan.lamb@gmail.com>
2016-11-23 21:32:33 -0500

    Add a button dropdown

--- sharewithfriends/public/javascripts/angularApp.js
+++ sharewithfriends/public/javascripts/angularApp.js
@@ -123,25 +123,26 @@ app.controller('PostsCtrl', [
   		};
 	}]);
 
-app.controller('ButtonsCtrl', function($scope) {
-	$scope.singleModel = 1;
+app.controller('DropDownCtrl', function($scope, $log) {
+	$scope.items = [
+    'The first choice!',
+    'And another choice for you.',
+    'but wait! A third!'
+  ];
 
-  $scope.radioModel = 'Middle';
-
-  $scope.checkModel = {
-    left: false,
-    middle: true,
-    right: false
+  $scope.status = {
+    isopen: false
   };
 
-  $scope.checkResults = [];
+  $scope.toggled = function(open) {
+    $log.log('Dropdown is now: ', open);
+  };
 
-  $scope.$watchCollection('checkModel', function () {
-    $scope.checkResults = [];
-    angular.forEach($scope.checkModel, function (value, key) {
-      if (value) {
-        $scope.checkResults.push(key);
-      }
-    });
-  });
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+
+  $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
 });
