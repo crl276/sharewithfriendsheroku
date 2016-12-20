f870d059d6806971b3f4ca16082c860cac0e4342
Callan Lamb <callan.lamb@gmail.com>
2016-12-19 21:55:06 -0500

    Add users and login ability

--- sharewithfriends/public/javascripts/angularApp.js
+++ sharewithfriends/public/javascripts/angularApp.js
@@ -1,147 +1,280 @@
 var app = angular.module('shareWithFriends', ['ui.router', 'ui.bootstrap']);
 
-app.factory('posts', ['$http', function($http) {
+app.factory('auth', ['$http', '$window',
+function($http, $window) {
+	var auth = {};
+
+	auth.saveToken = function(token) {
+		$window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function() {
+		return $window.localStorage['flapper-news-token'];
+	}
+
+	auth.isLoggedIn = function() {
+		var token = auth.getToken();
+
+		if (token) {
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.exp > Date.now() / 1000;
+		} else {
+			return false;
+		}
+	};
+
+	auth.currentUser = function() {
+		if (auth.isLoggedIn()) {
+			var token = auth.getToken();
+			var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+			return payload.username;
+		}
+	};
+
+	auth.register = function(user) {
+		return $http.post('/register', user).success(function(data) {
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logIn = function(user) {
+		return $http.post('/login', user).success(function(data) {
+			auth.saveToken(data.token);
+		});
+	};
+
+	auth.logOut = function() {
+		$window.localStorage.removeItem('flapper-news-token');
+	};
+
+	return auth;
+}]);
+
+app.factory('posts', ['$http', 'auth',
+function($http, auth) {
 	var o = {
-		posts: []
+		posts : []
 	};
 
 	o.getAll = function() {
-		return $http.get('/posts').success(function(data){
+		return $http.get('/posts').success(function(data) {
 			angular.copy(data, o.posts);
 		});
 	};
-
+	//now we'll need to create new posts
+	//uses the router.post in index.js to post a new Post mongoose model to mongodb
+	//when $http gets a success back, it adds this post to the posts object in
+	//this local factory, so the mongodb and angular data is the same
+	//sweet!
 	o.create = function(post) {
-		return $http.post('/posts', post).success(function(data){
-			o.posts.push(data);
-		});
+	  return $http.post('/posts', post, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    o.posts.push(data);
+	  });
 	};
-
-	o.deletePost = function(post) {
-		return $http.delete('/posts/' + post._id).success(function(status){
-			var index = o.posts.indexOf(post);
-			o.posts.splice(index,1);
-		});
-	};
-
+	
 	o.upvote = function(post) {
-  		return $http.put('/posts/' + post._id + '/upvote')
-  		  .success(function(data){
-      		post.upvotes += 1;
-    	});
+	  return $http.put('/posts/' + post._id + '/upvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    post.upvotes += 1;
+	  });
 	};
-
+	//downvotes
 	o.downvote = function(post) {
-		return $http.put('/posts/' + post._id + '/downvote')
-			.success(function(data){
-				post.upvotes -= 1;
-			});
+	  return $http.put('/posts/' + post._id + '/downvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    post.upvotes -= 1;
+	  });
 	};
-
+	//grab a single post from the server
 	o.get = function(id) {
-		return $http.get('/posts/' + id).then(function(res){
-    		return res.data;
-  		});
+		//use the express route to grab this post and return the response
+		//from that route, which is a json of the post data
+		//.then is a promise, a kind of newly native thing in JS that upon cursory research
+		//looks friggin sweet; TODO Learn to use them like a boss.  First, this.
+		return $http.get('/posts/' + id).then(function(res) {
+			return res.data;
+		});
 	};
-
+	//comments, once again using express
 	o.addComment = function(id, comment) {
- 		return $http.post('/posts/' + id + '/comments', comment);
+	  return $http.post('/posts/' + id + '/comments', comment, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  });
 	};
-
-	o.upvoteComment = function(post, comment){
-		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote').success(function(data){
-				comment.upvotes += 1;
-			});
-	};
-
+	
+	o.upvoteComment = function(post, comment) {
+	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    comment.upvotes += 1;
+	  });
+	};	
+	//downvote comments
+	//I should really consolidate these into one voteHandler function
+	o.downvoteComment = function(post, comment) {
+	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/downvote', null, {
+	    headers: {Authorization: 'Bearer '+auth.getToken()}
+	  }).success(function(data){
+	    comment.upvotes -= 1;
+	  });
+	};	
 	return o;
 }]);
 
-app.config([
-	'$stateProvider',
-	'$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
-		$stateProvider
-			.state('home', {
-				url: '/home',
-				templateUrl: '/home.html',
-				controller: 'MainCtrl',
-				resolve: {
-					postPromise: ['posts', function(posts) {
-						return posts.getAll();
-					}]
-				}
-			})
-			.state('about', {
-				url: '/about',
-				templateUrl: '/about.html',
-				controller: 'AboutCtrl'
-			})
-			.state('posts', {
-  				url: '/posts/{id}',
-  				templateUrl: '/posts.html',
-  				controller: 'PostsCtrl',
-  					resolve: {
-    					post: ['$stateParams', 'posts', function($stateParams, posts) {
-      					return posts.get($stateParams.id);
-    				}]
-  				}
-			});
 
-		$urlRouterProvider.otherwise('home');
-	}]);
+app.config(['$stateProvider', '$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
 
-app.controller('MainCtrl', [
-	'$scope',
-	'posts',
-	function($scope, posts){
-		$scope.posts = posts.posts;
-		$scope.addPost = function() {
-			if(!$scope.title || $scope.title === '') { return; }
-			posts.create({
-				title: $scope.title, 
-				link: $scope.link,
-			});
-			$scope.title = '';
-			$scope.link = '';
-		};
-		$scope.incrementUpvotes = function(post) {
-  			posts.upvote(post);
-		};
+	$stateProvider.state('home', {
+		url : '/home',
+		templateUrl : '/home.html',
+		controller : 'MainCtrl',
+		resolve : {
+			postPromise : ['posts',
+			function(posts) {
+				return posts.getAll();
+			}]
 
-		$scope.downvote = function(post) {
-			posts.downvote(post);
-		};
+		}
+	}).state('posts', {
+		url : '/posts/:id',
+		templateUrl : '/posts.html',
+		controller : 'PostsCtrl',
+		resolve : {
+			post : ['$stateParams', 'posts',
+			function($stateParams, posts) {
+				return posts.get($stateParams.id);
+			}]
 
-		$scope.deletePost = function(post) {
-			posts.deletePost(post);
-		};
-	}]);
+		}
+	}).state('login', {
+		url : '/login',
+		templateUrl : '/login.html',
+		controller : 'AuthCtrl',
+		onEnter : ['$state', 'auth',
+		function($state, auth) {
+			if (auth.isLoggedIn()) {
+				$state.go('home');
+			}
+		}]
 
-app.controller('PostsCtrl', [
-	'$scope',
-	'posts',
-	'post',
-	function($scope, posts, post) {
-		$scope.post = post;
-		$scope.addComment = function(){
- 			if($scope.body === '') { return; }
-  			posts.addComment(post._id, {
-    			body: $scope.body,
-    			author: 'user',
-  			}).success(function(comment) {
-    			$scope.post.comments.push(comment);
-  			});
-  		$scope.body = '';
-		};
-		$scope.upvote = function(comment) {
-  			posts.upvoteComment(post, comment);
-  		};
-	}]);
+	}).state('register', {
+		url : '/register',
+		templateUrl : '/register.html',
+		controller : 'AuthCtrl',
+		onEnter : ['$state', 'auth',
+		function($state, auth) {
+			if (auth.isLoggedIn()) {
+				$state.go('home');
+			}
+		}]
+
+	});
+
+	$urlRouterProvider.otherwise('home');
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', 'auth',
+function($scope, posts, auth) {
+	$scope.posts = posts.posts;
+	$scope.isLoggedIn = auth.isLoggedIn;
+	
+	$scope.title = '';
+
+	$scope.addPost = function() {
+		if ($scope.title === '') {
+			return;
+		}
+		posts.create({
+			title : $scope.title,
+			link : $scope.link,
+		});
+		//clear the values
+		$scope.title = '';
+		$scope.link = '';
+	};
+
+	$scope.upvote = function(post) {
+		//our post factory has an upvote() function in it
+		//we're just calling this using the post we have
+		console.log('Upvoting:' + post.title + "votes before:" + post.upvotes);
+		posts.upvote(post);
+	};
+	$scope.downvote = function(post) {
+		posts.downvote(post);
+	};
+}]);
+
+
+app.controller('PostsCtrl', ['$scope', 'posts', 'post', 'auth',
+function($scope, posts, post, auth) {
+	$scope.post = post;
+	$scope.isLoggedIn = auth.isLoggedIn;
+
+	$scope.addComment = function() {
+		if ($scope.body === '') {
+			return;
+		}
+		posts.addComment(post._id, {
+			body : $scope.body,
+			author : 'user'
+		}).success(function(comment) {
+			$scope.post.comments.push(comment);
+		});
+		$scope.body = '';
+	};
+	$scope.upvote = function(comment) {
+		posts.upvoteComment(post, comment);
+	};
+
+	$scope.downvote = function(comment) {
+		posts.downvoteComment(post, comment);
+	};
+
+}]);
 
 app.controller('AboutCtrl', ['$scope', function($scope){
 	var ctr = this;
 	ctr.text = "About Test";
+}]);
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
 }]);
 
 app.controller('DropDownCtrl', function($scope, $log) {
